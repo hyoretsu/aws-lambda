07f254c63613c891f999df4f1734e48161fd78dc
Aran Leite <hyoretsu@gmail.com>
2024-03-09 00:33:38 -0300

    Prettier deploy script

--- upload.sh
+++ upload.sh
@@ -7,7 +7,10 @@ shopt -s nullglob
 for project in build/*
 do
 	name="${project:6}"
+	echo "Deploying '$name'"
 	zip -r dist/$name.zip -j $project/index.js
+	echo "Uploading to AWS..."
+	echo ""
 	aws lambda update-function-code --function-name $name --zip-file fileb://dist/$name.zip
 done
 shopt -u nullglob
