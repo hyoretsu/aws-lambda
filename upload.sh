ee6fc2650ac7ed28f2affac4d04049eef9397edb
Aran Leite <hyoretsu@gmail.com>
2024-03-08 23:57:49 -0300

    Fix deploy

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ shopt -s nullglob
 for file in build/*
 do
 	name="${file:6:-3}"
-	zip -r dist/$name.zip $file
+	zip -r dist/$name.zip -j $file
 	aws lambda update-function-code --function-name $name --zip-file fileb://dist/$name.zip
 done
 shopt -u nullglob
