b897e9d21cd31c94a2fa2191cca87c628b1c4371
Aran Leite <hyoretsu@gmail.com>
2024-03-17 21:22:53 -0300

    Better upload script

--- upload.sh
+++ upload.sh
@@ -4,13 +4,11 @@ rm -rf dist
 mkdir dist
 
 shopt -s nullglob
-for project in build/*
-do
-	name="${project:6}"
+for project in $@; do
+	name="${project:4:-3}"
 	echo "Deploying '$name'"
-	zip -r dist/$name.zip -j $project/index.js
-	echo "Uploading to AWS..."
-	echo ""
+	zip -jr "dist/$name.zip" "build/$name/index.js"
+	echo -e "Uploading to AWS...\n"
 	aws lambda update-function-code --function-name $name --zip-file fileb://dist/$name.zip
 done
 shopt -u nullglob
