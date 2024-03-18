c1b27c07e7a1e4dc662211edbd45a2dc628116f5
Aran Leite <hyoretsu@gmail.com>
2024-03-18 01:01:47 -0300

    Fix upload script

--- upload.sh
+++ upload.sh
@@ -2,12 +2,13 @@
 
 rm -rf dist
 mkdir dist
+touch a.txt
 
 shopt -s nullglob
-for project in $@; do
-	name="${project:4:-3}"
-	echo "Deploying '$name'"
-	zip -jr "dist/$name.zip" "build/$name/index.js"
+for path in "$@"; do
+	project=$(file=${path##*/}; echo ${file%.*})
+	echo "Deploying '$project'"
+	zip -jr "dist/$project.zip" "build/$project/index.js"
 	echo -e "Uploading to AWS...\n"
 	aws lambda update-function-code --function-name $name --zip-file fileb://dist/$name.zip
 done
